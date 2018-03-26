3ca76ad572d6b52116f41f61bf67b30c87d494ea
Adam <lucia.adam@gmail.com>
2018-03-26 18:36:58 -0400

    google auth works

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -20,8 +20,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
     def create
-      byebug
       auth_params = request.env["omniauth.auth"]
+      byebug
       provider = AuthenticationProvider.where(name: auth_params.provider).first
       authentication = provider.user_authentications.where(uid: auth_params.uid).first
       existing_user = current_user || User.where('email = ?', auth_params['info']['email']).first
